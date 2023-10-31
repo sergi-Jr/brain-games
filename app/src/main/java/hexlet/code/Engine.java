19fe3b61112daa2b116b213e87bc41c8c7ee0b72
sergi-jr <burashnikov.sergei1@mail.ru>
2023-10-31 22:48:54 +0400

    Add Engine class

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,4 +1,17 @@
 package hexlet.code;
 
+import hexlet.code.abstracts.GameBase;
+
+/**
+ * Utility class provides games execution
+ */
 public class Engine {
+    /**
+     * Method that launches the game
+     * @param game Abstract class GameBase object
+     */
+    public static void execute(GameBase game) {
+        Greeter.greet(game.getScanner());
+        game.start(Greeter.getName());
+    }
 }
