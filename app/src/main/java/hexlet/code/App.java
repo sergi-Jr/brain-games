b661dcb1b655dfdf78b66b70e65c20342b752745
sergi-jr <burashnikov.sergei1@mail.ru>
2023-11-01 20:59:16 +0400

    Refactor App class: add switcher method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,21 +3,28 @@ package hexlet.code;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
 import hexlet.code.games.GCD;
+import hexlet.code.games.Prime;
 import hexlet.code.games.Progression;
 
 import java.util.Scanner;
 
 public class App {
+    private final static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("Please enter the game number and press Enter.");
-        System.out.print("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n0 - Exit\n");
+        System.out.print("1 - Greet\n2 - Even\n3 - Calc\n4 - GCD\n5 - Progression\n6 - Prime\n0 - Exit\n");
 
         String choice = scanner.nextLine();
 
         System.out.println("Your choice: " + choice);
+        gameSwitcher(choice);
+    }
 
+    /**
+     * Method launches required game by user choice
+     * @param choice User entered choice
+     */
+    private static void gameSwitcher(String choice) {
         switch (choice) {
             case ("1"): //greet message
                 Greeter.greet(scanner);
@@ -36,6 +43,9 @@ public class App {
             case ("5"): //Progression game choice
                 Progression progressionGame = new Progression(scanner);
                 Engine.execute(progressionGame);
+            case ("6"): //Prime game choice
+                Prime primeGame = new Prime(scanner);
+                Engine.execute(primeGame);
             default: //Exit choice or unexpected input string
                 break;
         }
