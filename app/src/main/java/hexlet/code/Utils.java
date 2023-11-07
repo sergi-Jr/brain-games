ae3baf7191fe0503fa19fdc42b5d93aa7d205e2c
sergi-Jr <burashnikov.sergei1@mail.ru>
2023-11-07 23:00:09 +0400

    Add Utils class

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,4 +1,10 @@
 package hexlet.code;
 
+import java.util.Random;
+
 public class Utils {
+    public static int getRandomInt(int lowerBorder, int upperBorder) {
+        Random rnd = new Random();
+        return rnd.nextInt(lowerBorder, upperBorder);
+    }
 }
